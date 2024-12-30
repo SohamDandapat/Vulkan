622ebb1e935a7ccebddf65d73b79e06d8f58dda6
Soham Dandapat <you@example.com>
2024-12-30 17:09:33 +0530

    Vulkan experiment

---
+++ Triangle.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define GLFW_INCLUDE_VULKAN
+
+void initVulakn();
+void mainloop();
+void cleanup();
+
+
+int main()
+{
+	initVulakn();
+	mainloop();
+	cleanup();
+
+
+    retrun 0;
+}
+
+
+void initVulakn()
+{
+
+
+}
+void mainloop()
+{
+
+}
+void cleanup()
+{
+
+
+
+}
